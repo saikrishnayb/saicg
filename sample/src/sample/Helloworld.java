7d6eaf85c61bdb957de64bdabb9438ede5a544a5
yasaikri <yasaikri@DIN37002067.corp.capgemini.com>
2022-06-07 18:17:45 +0530

    FIRST COMMIT

---
+++ sample/src/sample/Helloworld.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello sample application");
+
+	}
+
+}
---
+++ sample/src/sample/Main.java
@@ -0,0 +1,22 @@
+package sample;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Main {
+public static void main (String args[]) throws Exception{
+	Map<String,Integer> hm=new HashMap<String,Integer>();
+	hm.put("clothes",200);
+	hm.put("grocery", 400);
+	hm.put("transportation", 1000);
+	hm.put("rent",1150);
+	hm.put("mislaneous", 100);
+	System.out.println("map before sorting: " + hm);
+	
+			
+	
+}
+}
